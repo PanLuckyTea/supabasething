cc35aec4f8cdf09e2b35556cdc22fa399892bff2
PanLuckyTea <dziedzicki@student.agh.edu.pl>
2026-06-11 23:10:35 +0200

    im tired

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // WAŻNE: Wpisz nazwę swojego repozytorium GitHub zamiast tekstu poniżej,
-  // na przykład: base: '/supabase-blog-project/'
-  base: '/nazwa-twojego-repozytorium/', 
+  base: '/supabasething/',
 });
